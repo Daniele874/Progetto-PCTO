c7d2735aec3046fb3e6f8d6ffedaf9de5a195902
Daniele874 <sdaniele.cilla@itis.pr.it>
2026-06-10 17:47:57 +0200

    Aggiunta del progetto MCV

---
+++ WebApplicationF1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationF1.Models;
+
+namespace WebApplicationF1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Piloti()
+        {
+            return View();
+        }
+
+        public IActionResult Scuderie()
+        {
+            return View();
+        }
+
+        public IActionResult Campionati()
+        {
+            return View();
+        }
+
+    
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
